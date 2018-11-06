6c6edb77ac59b172b43edd53ae8edefd66e9bd5a
Eduardo de Souza <eduardohsfreitas86@gmail.com>
2018-11-06 08:06:55 -0300

    Eleventh Performance/Program made in C#

---
+++ EleventhPerformance/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace EleventhPerformance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double batT, jogT, tempoT;
+            int vezes;
+
+            tempoT = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            vezes = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            batT = tempoT;
+            for(int i=0; i<vezes; i++)
+            {
+                jogT = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                batT = batT - jogT;
+                if(batT < 0.0)
+                {
+                    Console.WriteLine("RELOAD!");
+                }
+                else
+                {
+                    Console.WriteLine(batT);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
